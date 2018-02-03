33353b449399ade5bb1e2f86372805b5563a6c5c
Miroslav Ferienčík <feriencik.miro@gmail.com>
2018-02-03 23:04:42 +0100

    WIP

--- Code/src/Robisk/ApplicationBundle/Controller/CommentController.php
+++ Code/src/Robisk/ApplicationBundle/Controller/CommentController.php
@@ -129,4 +129,65 @@ class CommentController extends BaseController
         return $response;
     }
 
+    public function userPresentationForumAction($id, $presentationId){
+        $presentationManager = $this->get('manager_presentation');
+        $commentManager = $this->get('manager_user_presentation_comment');
+        $subjectManager = $this->get('manager_subject');
+        $request = $this->get('request');
+
+        $subject = $subjectManager->findOneBy(array('id' => $id));
+        $presentation = $presentationManager->findOneBy(array('id' => $presentationId));
+        $comment = $commentManager->create();
+        $user = $this->getUser();
+
+        $form = $this->createForm(new CommentType(), $comment, array('csrf_protection' => false));
+
+        $url = $this->generateUrl('route_subject_presentation_forum',
+            array(
+                'id'             => $id,
+                'presentationId' => $presentationId
+            ));
+
+        if ($request->isMethod("POST")) {
+            $form->handleRequest($request);
+            if ($form->isValid()) {
+
+                $comment->setUser($this->getUser());
+                $comment->setPresentation($presentation);
+                $comment->setDate(new \DateTime("now"));
+
+                $commentManager->update($comment);
+
+                $response = new RedirectResponse($url);
+                return $response;
+            }
+        }
+
+        return $this->render(
+            'RobiskApplicationBundle:Comment:userPresentationUserForum.html.twig',
+            array(
+                'presentation' => $presentation,
+                'subject' => $subject,
+                'form' => $form->createView(),
+                'url' => $url,
+                'user' => $user
+            )
+        );
+    }
+
+    public function userPresentationUserForumDeleteAction($id, $presentationId, $commentId)
+    {
+        $commentManager = $this->get('manager_user_presentation_comment');
+        $comment = $commentManager->findOneBy(array('id' => $commentId));
+        $commentManager->delete($comment);
+
+        $url = $this->generateUrl('route_subject_presentation_forum',
+            array(
+                'id' => $id,
+                'presentationId' => $presentationId
+            ));
+        $response = new RedirectResponse($url);
+        return $response;
+    }
+
 }
